6579dc98aef0911c770ff2c5348c26bf7b77b644
Jena <jenagansivakumar@gmail.com>
2024-02-05 19:46:22 +0100

    got the links centred to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,21 @@
 import "../../src/index.css";
+
 export const Navbar = () => {
   return (
     <div className="navbar-container">
       <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/pricing">Pricing</a>
-        <a href="/contact">Contact</a>
+        <a href="/">Poblacht Barbershop</a>
+        <ul>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/pricing">Pricing</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
       </nav>
     </div>
   );
