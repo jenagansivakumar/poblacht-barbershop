d569bf310b553671d8d48a2ee279844ebd6de032
Jena <jenagansivakumar@gmail.com>
2024-02-06 14:54:03 +0100

    adding about, home and pricing routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,19 @@ import "../../src/index.css";
 
 export const Navbar = () => {
   return (
-    <div className="navbar-container">
-      <nav>
-        <a href="/">Poblacht Barbershop</a>
-        <ul>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/pricing">Pricing</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className="navbar-container">
+      <a href="/">Poblacht Barbershop</a>
+      <ul>
+        <li className="active">
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/pricing">Pricing</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
   );
 };
